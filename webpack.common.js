b32cfc1566761924bacf96072d336139c15804d2
luxeivan <luxeivan@gmail.com>
2022-06-29 12:56:19 +0300

    123

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/ahj_collapse/',
+    publicPath: '/ahj_chatFront/',
   },
   module: {
     rules: [
